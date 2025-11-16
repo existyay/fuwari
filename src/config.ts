448f0ba0f9e9ef8526ac7e618136183afefb8c57
liujam <2684023076@qq.com>
2025-11-16 22:50:01 +0800

    update

--- src/config.ts
+++ src/config.ts
@@ -12,7 +12,7 @@ export const siteConfig: SiteConfig = {
 	subtitle: "☁",
 	lang: "zh_CN", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
-		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
+		hue: 240, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
 		fixed: false, // Hide the theme color picker for visitors
 	},
 	banner: {
@@ -37,10 +37,10 @@ export const siteConfig: SiteConfig = {
 		//   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
 		// }
 		{
-       		src: 'https://q2.qlogo.cn/headimg_dl?dst_uin=2973517380&spec=5',    // Path of the favicon, relative to the /public directory
-       		//theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
-       		sizes: '128x128',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
-   		}
+			src: "https://q2.qlogo.cn/headimg_dl?dst_uin=2973517380&spec=5", // Path of the favicon, relative to the /public directory
+			//theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
+			sizes: "128x128", // (Optional) Size of the favicon, set only if you have favicons of different sizes
+		},
 	],
 };
 
