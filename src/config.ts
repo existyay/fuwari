68e8124f51613acfe07863464bebc4ee9d38913d
liujam <2684023076@qq.com>
2025-11-17 08:22:26 +0800

    update

--- src/config.ts
+++ src/config.ts
@@ -58,7 +58,7 @@ export const navBarConfig: NavBarConfig = {
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+	avatar: "assets/images/avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "Xiao Bai",
 	bio: "You want you do.",
 	links: [
--- src/content/posts/优化对应的CDN访问.md
+++ src/content/posts/优化对应的CDN访问.md
@@ -9,3 +9,7 @@ draft: false
 lang: 'zh-CN'
 ---
 # 尝试使用不同的page代理和CDN来优化访问该站点的体验
+
+就结果而言，直接使用cloudflare的效果是最佳的，其国内的CDN现在优于其他不同的CDN静态页面托管平台
+
+如:Vercel、Netlify而其中的Vercel国内至少可以进行访问，而Netlify现在默认居然部署在AM东部，导致部署的网站直接无法访问。
